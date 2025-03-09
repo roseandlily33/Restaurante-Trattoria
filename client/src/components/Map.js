8d6fd124abe31512e1c32045bbd685acef0367f2
Victoria Benoit <vrose834@gmail.com>
2025-03-08 20:18:24 -0400

    trying an upload commit

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,61 +1,61 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+// import React, { Component } from 'react';
+// // import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
-const mapStyles = {
-    margin: 'auto',
-    width: '70%',
-    height: '50%',
-    position: 'relative',
-    marginTop: '50px', 
+// const mapStyles = {
+//     margin: 'auto',
+//     width: '70%',
+//     height: '50%',
+//     position: 'relative',
+//     marginTop: '50px', 
 
-};
+// };
 
-export class MapContainer extends Component {
+// export class MapContainer extends Component {
 
-    state = {
-        showingInfoWindow: false,
-        activeMarker: {},
-        selectedPlace: {}
-    };
+//     state = {
+//         showingInfoWindow: false,
+//         activeMarker: {},
+//         selectedPlace: {}
+//     };
  
-    render() {
-      return (
-        <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
-          initialCenter={
-            {
-              lat: 45.3065318317676, 
-              lng: -66.08558591878472
-            } 
+//     render() {
+//       return (
+//         <Map
+//           google={this.props.google}
+//           zoom={14}
+//           style={mapStyles}
+//           initialCenter={
+//             {
+//               lat: 45.3065318317676, 
+//               lng: -66.08558591878472
+//             } 
             
-          }
-        >
+//           }
+//         >
 
-            <Marker
-                onClick={this.onMarkerClick}
-                name={'Our Location'}
-            />
-            <InfoWindow
-                marker={this.state.activeMarker}
-                visible={this.state.showingInfoWindow}
-                onClose={this.onClose}
-            >
-            <div>
-                <h4>{this.state.selectedPlace.name}</h4>
-            </div>
-            </InfoWindow>
-        </Map>
+//             <Marker
+//                 onClick={this.onMarkerClick}
+//                 name={'Our Location'}
+//             />
+//             <InfoWindow
+//                 marker={this.state.activeMarker}
+//                 visible={this.state.showingInfoWindow}
+//                 onClose={this.onClose}
+//             >
+//             <div>
+//                 <h4>{this.state.selectedPlace.name}</h4>
+//             </div>
+//             </InfoWindow>
+//         </Map>
         
-      );
-    }
-};
+//       );
+//     }
+// };
 
 
   
-export default GoogleApiWrapper(
-    (props) => ({
-        apiKey: 'AIzaSyC2i40CPJXEwGo0uFtlUatTQ2iwv9Z9Mkg'
-    })
-)(MapContainer);
+// export default GoogleApiWrapper(
+//     (props) => ({
+//         apiKey: 'AIzaSyC2i40CPJXEwGo0uFtlUatTQ2iwv9Z9Mkg'
+//     })
+// )(MapContainer);
